e921bbbf9529c18c74a7f0f54904f7c0ee860bbc
Chacaponquin <hectorangel2001@gmail.com>
2022-08-15 22:30:08 -0400

    Update

--- src/socket/SocketTasks.ts
+++ src/socket/SocketTasks.ts
@@ -6,6 +6,8 @@ export const SocketTasks = (socket: Socket) => {
   socket.on("disconnect", () => console.log("Client Desconnected"));
   socket.on("connection", () => console.log("Hola"));
 
+  setInterval(() => socket.emit("Hola"), 1000);
+
   socket.on(
     "createDatasets",
     async (args) => await createDatasets(socket, args)
