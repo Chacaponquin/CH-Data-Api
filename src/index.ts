bf2118525baaf7a1493628ffdbc69a008d09bbd3
Chacaponquin <hectorangel2001@gmail.com>
2022-08-23 22:25:26 -0400

    Update

--- src/index.ts
+++ src/index.ts
@@ -72,7 +72,6 @@ passport.use(
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     },
     function (accessToken: any, refreshToken: any, profile: any, cb: any) {
-      console.log(profile._json);
       cb(null, profile._json);
     }
   )
--- src/routes/authentication/tasks/githubAuth.ts
+++ src/routes/authentication/tasks/githubAuth.ts
@@ -27,6 +27,8 @@ export const githubAuthRoute = async (req: Request, res: Response) => {
       );
     }
 
+    console.log(token);
+
     res
       .status(200)
       .cookie("jwt", token)
